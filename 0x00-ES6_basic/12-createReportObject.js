e4186c27955a5d61a637289f1d5a9cfda04513f3
wand3 <godswillbaiyewu@gmail.com>
2023-07-09 09:37:50 -0700

    more ES6-basic

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,12 @@
+export default function createReportObject(employeesList) {
+  const obj = {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments() {
+      return Object.keys(employeesList).length;
+    },
+  };
+
+  return obj;
+}
